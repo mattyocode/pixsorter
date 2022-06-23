269ee52a34c8255d5ab3e0a9cbd2d3d44f94f09b
Matt <63904102+mattyocode@users.noreply.github.com>
2022-06-23 23:45:28 +0100

    add further tests

--- src/__tests__/utils/algos.test.tsx
+++ src/__tests__/utils/algos.test.tsx
@@ -5,11 +5,11 @@ describe("Bubble sort tests", () => {
   const pixelIndexLength = 4;
   // compare red pixel value in RGBA value array
   const compareFirst = (array: Uint8ClampedArray, index: number): number => {
-    return array[0];
+    return array[index];
   };
-  test("sorts two subarrays but 0th value", () => {
-    const testArray = new Uint8ClampedArray([1, 0, 0, 0, 0, 0, 0, 0]);
-    const sortPositionStart = 4;
+  test("sorts two pixels based on red value (first of 4)", () => {
+    let testArray = new Uint8ClampedArray([1, 0, 0, 0, 0, 0, 0, 0]);
+    const sortPositionStart = testArray.length;
     const endSortPosition = bubbleSortHelper(
       testArray,
       compareFirst,
@@ -17,5 +17,32 @@ describe("Bubble sort tests", () => {
       pixelIndexLength
     );
     expect(endSortPosition).toEqual(0);
+    expect(testArray).toEqual(new Uint8ClampedArray([0,0,0,0,1,0,0,0]))
+  });
+  test("sorts remaining pixel values as well as selected (RGBA all move)", () => {
+    let testArray = new Uint8ClampedArray([1, 7, 7, 7, 0, 8, 8, 8]);
+    const sortPositionStart = testArray.length;
+    const endSortPosition = bubbleSortHelper(
+      testArray,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(0);
+    expect(testArray).toEqual(new Uint8ClampedArray([0,8,8,8,1,7,7,7]))
+  });
+  test("sorts three pixels", () => {
+    let testArray = new Uint8ClampedArray(
+      [2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0]
+    );
+    const sortPositionStart = testArray.length;
+    const endSortPosition = bubbleSortHelper(
+      testArray,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(0);
+    expect(testArray).toEqual(new Uint8ClampedArray([0,0,0,0,1,0,0,0]))
   });
 });
