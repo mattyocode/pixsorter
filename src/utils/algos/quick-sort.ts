6e8e732bcfa6035431f1a5fcdd800092e7917ad6
Matt <63904102+mattyocode@users.noreply.github.com>
2022-06-26 16:16:01 +0100

    add quicksort

---
+++ src/utils/algos/quick-sort.ts
@@ -0,0 +1,55 @@
+import swap from "./pixel-swap";
+
+export const quickSortHelper = (
+  array: Uint8ClampedArray,
+  compare: (array: Uint8ClampedArray, index: number) => number,
+  pixelIdxLength: number,
+  startIdx: number,
+  endIdx: number,
+) => {
+  if (startIdx >= endIdx) {
+    return;
+  }
+  
+  let pivotIdx = startIdx;
+  let leftIdx = pivotIdx + pixelIdxLength;
+  let rightIdx = endIdx - pixelIdxLength;
+
+  while (leftIdx <= rightIdx) {
+    if(compare(array, leftIdx) > compare(array, pivotIdx) &&
+      compare(array, rightIdx) < compare(array, pivotIdx)) {
+        swap(leftIdx, rightIdx, array)
+      }
+    if(compare(array, leftIdx) <= compare(array, pivotIdx)) {
+      leftIdx += pixelIdxLength;
+    }
+    if(compare(array, rightIdx) >= compare(array, pivotIdx)) {
+      rightIdx -= pixelIdxLength;
+    }
+  }
+  swap(pivotIdx, rightIdx, array);
+
+  let leftSubArrayIsSmaller = rightIdx - pixelIdxLength - startIdx < endIdx - (rightIdx + pixelIdxLength);
+  if (leftSubArrayIsSmaller) {
+    quickSortHelper(array, compare, pixelIdxLength, startIdx, rightIdx - pixelIdxLength);
+    quickSortHelper(array, compare, pixelIdxLength, rightIdx + pixelIdxLength, endIdx);    
+  } else {
+    quickSortHelper(array, compare, pixelIdxLength, rightIdx + pixelIdxLength, endIdx);
+    quickSortHelper(array, compare, pixelIdxLength, startIdx, rightIdx - pixelIdxLength);
+  }
+};
+
+const quickSort = (
+  array: Uint8ClampedArray,
+  sortedCallback: () => void,
+  compare: (array: Uint8ClampedArray, index: number) => number,
+  sortPosition: number | null,
+  pixelIdxLength: number = 4,
+) => {
+  const startIdx = 0;
+  const endIdx = array.length;
+  quickSortHelper(array, compare, pixelIdxLength, startIdx, endIdx);
+  sortedCallback();
+};
+
+export default quickSort;
