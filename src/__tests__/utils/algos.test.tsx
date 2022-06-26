545da0cf9d875952b52024e1a4315374c2ff77da
Matt <63904102+mattyocode@users.noreply.github.com>
2022-06-26 13:47:59 +0100

    add tests for selection sort

--- src/__tests__/utils/algos.test.tsx
+++ src/__tests__/utils/algos.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { bubbleSort, insertionSort } from "../../utils/algos";
+import { bubbleSort, insertionSort, selectionSort } from "../../utils/algos";
 import { bubbleSortHelper } from "../../utils/algos/bubble-sort";
 import { insertionSortHelper } from "../../utils/algos/insertion-sort";
+import { selectionSortHelper } from "../../utils/algos/selection-sort";
 
 // ---- Pixel comparison functions ---- //
 // Compare red pixel value in RGBA value array
@@ -16,6 +17,10 @@ const compareSecond = (array: Uint8ClampedArray, index: number): number => {
 const compareThird = (array: Uint8ClampedArray, index: number): number => {
   return array[index + 2];
 };
+// Compare sum of RGB pixels
+const compareAll = (array: Uint8ClampedArray, index: number): number => {
+  return array[index] + array[index + 1] + array[index + 2];
+}
 
 const pixelIndexLength = 4;
 
@@ -116,6 +121,42 @@ describe("Bubble sort tests", () => {
       [9, 1, 9, 9, 8, 2, 8, 8, 7, 3, 7, 7]
     ))
   });
+  test("sort by third pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 8, 2, 8, 7, 7, 3, 7, 9, 9, 1, 9]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = bubbleSort(
+      testArray,
+      sortedCallback,
+      compareThird,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(0);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [9, 9, 1, 9, 8, 8, 2, 8, 7, 7, 3, 7]
+    ))
+  });
+  test("sort by combined pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 7, 6, 5, 12, 11, 10, 9, 4, 3, 2, 1]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = bubbleSort(
+      testArray,
+      sortedCallback,
+      compareAll,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(0);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [4, 3, 2, 1, 8, 7, 6, 5, 12, 11, 10, 9]
+    ))
+  });
 });
 
 describe("Insertion sort tests", () => {
@@ -211,6 +252,175 @@ describe("Insertion sort tests", () => {
     expect(endSortPosition).toEqual(testArray.length);
     expect(testArray).toEqual(new Uint8ClampedArray(
       [9, 1, 9, 9, 8, 2, 8, 8, 7, 3, 7, 7]
+    ))
+  });
+  test("sort by third pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 8, 2, 8, 7, 7, 3, 7, 9, 9, 1, 9]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = insertionSort(
+      testArray,
+      sortedCallback,
+      compareThird,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [9, 9, 1, 9, 8, 8, 2, 8, 7, 7, 3, 7]
+    ))
+  });
+  test("sort by combined pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 7, 6, 5, 12, 11, 10, 9, 4, 3, 2, 1]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = insertionSort(
+      testArray,
+      sortedCallback,
+      compareAll,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [4, 3, 2, 1, 8, 7, 6, 5, 12, 11, 10, 9]
+    ))
+  });
+});
+
+describe("Selection sort tests", () => {
+  const sortPositionStart = 0;
+  test("sort helper sorts two pixels based on red value (first of 4)", () => {
+    let testArray = new Uint8ClampedArray([1, 0, 0, 0, 0, 0, 0, 0]);
+    const endSortPosition = selectionSortHelper(
+      testArray,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(pixelIndexLength);
+    expect(testArray).toEqual(new Uint8ClampedArray([0,0,0,0,1,0,0,0]))
+  });
+  test("sort helper sorts remaining 3 pixel values as well as selected", () => {
+    let testArray = new Uint8ClampedArray([1, 9, 9, 9, 0, 10, 10, 10]);
+    const endSortPosition = selectionSortHelper(
+      testArray,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(pixelIndexLength);
+    expect(testArray).toEqual(new Uint8ClampedArray([0,10,10,10,1,9,9,9]))
+  });
+  test("sorts three pixels", () => {
+    let testArray = new Uint8ClampedArray(
+      [2, 8, 8, 8, 3, 7, 7, 7, 1, 9, 9, 9]
+    );
+    let sortedCallback = jest.fn()
+    const endSortPosition = selectionSort(
+      testArray,
+      sortedCallback,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [1, 9, 9, 9, 2, 8, 8, 8, 3, 7, 7, 7]
+    ))
+  });
+  test("sorts three pixels without sortPosition provided", () => {
+    let testArray = new Uint8ClampedArray(
+      [2, 8, 8, 8, 3, 7, 7, 7, 1, 9, 9, 9]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = selectionSort(
+      testArray,
+      sortedCallback,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [1, 9, 9, 9, 2, 8, 8, 8, 3, 7, 7, 7]
+    ))
+  });
+  test("sorts three digit numbers", () => {
+    let testArray = new Uint8ClampedArray(
+      [128, 200, 200, 200, 256, 300, 300, 300, 56, 100, 100, 100]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = selectionSort(
+      testArray,
+      sortedCallback,
+      compareFirst,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [56, 100, 100, 100, 128, 200, 200, 200, 256, 300, 300, 300]
+    ))
+  });
+  test("sort by second pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 2, 8, 8, 7, 3, 7, 7, 9, 1, 9, 9]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = selectionSort(
+      testArray,
+      sortedCallback,
+      compareSecond,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [9, 1, 9, 9, 8, 2, 8, 8, 7, 3, 7, 7]
+    ))
+  });
+  test("sort by third pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 8, 2, 8, 7, 7, 3, 7, 9, 9, 1, 9]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = selectionSort(
+      testArray,
+      sortedCallback,
+      compareThird,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [9, 9, 1, 9, 8, 8, 2, 8, 7, 7, 3, 7]
+    ))
+  });
+  test("sort by combined pixel value", () => {
+    let testArray = new Uint8ClampedArray(
+      [8, 7, 6, 5, 12, 11, 10, 9, 4, 3, 2, 1]
+    );
+    let sortedCallback = jest.fn()
+    const sortPositionStart = null;
+    const endSortPosition = selectionSort(
+      testArray,
+      sortedCallback,
+      compareAll,
+      sortPositionStart,
+      pixelIndexLength
+    );
+    expect(endSortPosition).toEqual(testArray.length);
+    expect(testArray).toEqual(new Uint8ClampedArray(
+      [4, 3, 2, 1, 8, 7, 6, 5, 12, 11, 10, 9]
     ))
   });
 });
