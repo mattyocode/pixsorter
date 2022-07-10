4716d9649ee7283119d0cc8b02354ae3678aee2d
Matt <63904102+mattyocode@users.noreply.github.com>
2022-07-10 13:24:00 +0100

    apply  wrapper style class

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -3,11 +3,13 @@ import { SidewaysSelector } from "../SidewaysSelector";
 
 import AlgoContext from "../../store/algo-context";
 
+import styles from "./Controls.module.scss";
+
 export function Controls() {
   const algoCtx = useContext(AlgoContext);
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <SidewaysSelector
         field="Using"
         values={algoCtx.algos}
@@ -22,6 +24,6 @@ export function Controls() {
         prevBtnHandler={algoCtx.prevSortBy}
         nextBtnHandler={algoCtx.nextSortBy}
       />
-    </>
+    </div>
   );
 }
