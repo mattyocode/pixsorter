753142a62aae6721c6ea4cf82408c08cb5bb9964
Matt <63904102+mattyocode@users.noreply.github.com>
2022-07-20 19:13:34 +0100

    basic algo info element added

--- src/components/SidewaysSelector/index.tsx
+++ src/components/SidewaysSelector/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef, Dispatch, SetStateAction } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
 import { ImageUIBtn } from "../Buttons";
 import { AlgoItemType, SortByItemType } from "../../store/algoData";
 
@@ -17,7 +19,60 @@ const FieldValue = ({ value, active }: { value: string; active: boolean }) => {
     }
   }, [active]);
 
-  return <li ref={optionRef}>{value}</li>;
+  return (
+    <li ref={optionRef}>
+      {value}
+      {/* {active && (
+        <div className={styles.fieldIcon}>
+          <Image
+            src="/icons/slow.svg"
+            height={25}
+            width={25}
+            alt="more information"
+            className={styles.fieldIcon}
+          />
+        </div>
+      )} */}
+    </li>
+  );
+};
+
+const fieldInfoVariants = {
+  closed: {
+    height: 0,
+  },
+  open: {
+    height: 200,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+const FieldInfo = ({
+  heading,
+  bodyCopy,
+  children,
+  ...restProps
+}: {
+  heading: string;
+  bodyCopy: string;
+  children: React.Component;
+  // imageSrc: string | null;
+}) => {
+  return (
+    <motion.div
+      initial="closed"
+      animate="open"
+      exit="closed"
+      variants={fieldInfoVariants}
+      className={styles.fieldInfoWrapper}
+      {...restProps}
+    >
+      <h3>{heading}</h3>
+      <p>{bodyCopy}</p>
+    </motion.div>
+  );
 };
 
 export function SidewaysSelector({
@@ -33,6 +88,7 @@ export function SidewaysSelector({
   prevBtnHandler: () => void;
   nextBtnHandler: () => void;
 }) {
+  const [infoOpen, setInfoOpen] = useState<boolean>(false);
   const decrementSelected = (e: React.MouseEvent) => {
     e.preventDefault();
     prevBtnHandler();
@@ -43,37 +99,63 @@ export function SidewaysSelector({
     nextBtnHandler();
   };
 
+  const toggleInfo = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setInfoOpen((prev) => !prev);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <label htmlFor="algorithms" className={styles.field}>
-        <h3>{field}</h3>
-      </label>
-      <div className={styles.optionsWrapper}>
-        <ImageUIBtn
-          src="/icons/left.svg"
-          alt="prev option"
-          width={25}
-          height={25}
-          clickHandler={decrementSelected}
-        />
-        <div className={styles.fieldWrapper}>
-          <ul>
-            {values.map((value, idx) => (
-              <FieldValue
-                key={`fieldValue-${value.value}-${idx}`}
-                value={value.label}
-                active={values[selectedIdx] === value}
-              />
-            ))}
-          </ul>
+      <div className={styles.selectorRow}>
+        <label htmlFor="fieldInput" className={styles.field}>
+          <h3>{field}</h3>
+        </label>
+        <div className={styles.optionsWrapper}>
+          <ImageUIBtn
+            src="/icons/left.svg"
+            alt="prev option"
+            width={25}
+            height={25}
+            clickHandler={decrementSelected}
+          />
+          <div className={styles.fieldWrapper}>
+            <ul>
+              {values.map((value, idx) => (
+                <FieldValue
+                  key={`fieldValue-${value.value}-${idx}`}
+                  value={value.label}
+                  active={values[selectedIdx] === value}
+                />
+              ))}
+            </ul>
+          </div>
+          <ImageUIBtn
+            src="/icons/right.svg"
+            alt="next option"
+            width={25}
+            height={25}
+            clickHandler={incrementSelected}
+          />
         </div>
-        <ImageUIBtn
-          src="/icons/right.svg"
-          alt="next option"
-          width={25}
-          height={25}
-          clickHandler={incrementSelected}
-        />
+        <div className={styles.actionIcon}>
+          <ImageUIBtn
+            src="/icons/info.svg"
+            alt="more information"
+            width={18}
+            height={18}
+            clickHandler={toggleInfo}
+          />
+        </div>
+      </div>
+      <div className={styles.infoRow}>
+        <AnimatePresence>
+          {infoOpen && (
+            <FieldInfo
+              heading="Avg. time: O(n²) | Avg Space: O(1)"
+              bodyCopy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempor ultrices enim, nec tincidunt nisi imperdiet sed. Morbi blandit sem in velit vulputate, suscipit interdum felis finibus. Sed suscipit tincidunt lorem, at rhoncus tellus rhoncus at. Vestibulum congue consectetur orci, sit amet pellentesque lacus vestibulum et."
+            />
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
