958d4c5d8da460327fb4b23fcee732541445a164
Matt <63904102+mattyocode@users.noreply.github.com>
2022-08-13 14:55:53 +0100

    merge sort working

--- src/utils/algos/merge-sort.ts
+++ src/utils/algos/merge-sort.ts
@@ -2,43 +2,45 @@ import copy from "./pixel-copy";
 import { SortAlgoTypes, SortDataTypes } from "./algoTypes";
 import { compareFnTypes } from "./pixel-comparison";
 
+// const sleep = (ms: number) => {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// };
+
 const mergeSort: SortAlgoTypes = (
   array,
   sortedCallback,
   compare,
   sortPosition,
   pixelIdxLength = 4,
-  renderLoops = 10
+  renderLoops = 1
 ) => {
   let width: number =
     typeof sortPosition === "number" ? sortPosition : pixelIdxLength;
   let length = array.length;
   for (let i = 0; i <= renderLoops; i++) {
-    while (width < length) {
-      let left: number = 0;
-      while (left < length) {
-        let right = Math.min(
-          left + (width * 2 - pixelIdxLength),
-          length - pixelIdxLength
-        );
-        let middle = Math.min(
-          left + width - pixelIdxLength,
-          length - pixelIdxLength
-        );
-        merge(array, left, right, middle, compare, pixelIdxLength);
-        left += width * 2;
-      }
-      width *= 2;
-    }
     if (width >= length) {
       sortedCallback();
       break;
     }
+    let left: number = 0;
+    while (left < length) {
+      let right = Math.min(
+        left + (width * 2 - pixelIdxLength),
+        length - pixelIdxLength
+      );
+      let middle = Math.min(
+        left + width - pixelIdxLength,
+        length - pixelIdxLength
+      );
+      merge(array, left, right, middle, compare, pixelIdxLength);
+      left += width * 2;
+    }
+    width *= 2;
   }
   return width;
 };
 
-const merge = (
+const merge = async (
   array: Uint8ClampedArray,
   left: number,
   right: number,
