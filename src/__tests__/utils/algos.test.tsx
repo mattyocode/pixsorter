b295497481caacc7206fb4a93403d785a8fb18ac
Matt <63904102+mattyocode@users.noreply.github.com>
2022-08-13 14:15:33 +0100

    merge sort tests passing

--- src/__tests__/utils/algos.test.tsx
+++ src/__tests__/utils/algos.test.tsx
@@ -4,27 +4,29 @@ import {
   insertionSort,
   selectionSort,
   quickSort,
+  mergeSort,
 } from "../../utils/algos";
 import { bubbleSortHelper } from "../../utils/algos/bubble-sort";
 import { insertionSortHelper } from "../../utils/algos/insertion-sort";
 import { selectionSortHelper } from "../../utils/algos/selection-sort";
 import { SortDataTypes } from "../../utils/algos/algoTypes";
+import { compareFnTypes } from "../../utils/algos/pixel-comparison";
 
 // ---- Pixel comparison functions ---- //
 // Compare red pixel value in RGBA value array
-const compareFirst = (array: Uint8ClampedArray, index: number): number => {
+const compareFirst: compareFnTypes = (array, index) => {
   return array[index];
 };
 // Compare blue pixel value in RGBA value array
-const compareSecond = (array: Uint8ClampedArray, index: number): number => {
+const compareSecond: compareFnTypes = (array, index) => {
   return array[index + 1];
 };
 // Compare green pixel value in RGBA value array
-const compareThird = (array: Uint8ClampedArray, index: number): number => {
+const compareThird: compareFnTypes = (array, index) => {
   return array[index + 2];
 };
 // Compare sum of RGB pixels
-const compareAll = (array: Uint8ClampedArray, index: number): number => {
+const compareAll: compareFnTypes = (array, index) => {
   return array[index] + array[index + 1] + array[index + 2];
 };
 
@@ -421,32 +423,6 @@ describe("Selection sort tests", () => {
 
 describe("Quick sort tests", () => {
   let testSortPosition: null | SortDataTypes = null;
-  // test("sort helper sorts two pixels based on red value (first of 4)", () => {
-  //   let testArray = new Uint8ClampedArray([1, 0, 0, 0, 0, 0, 0, 0]);
-  //   const startIdx = 0;
-  //   const endIdx = testArray.length;
-  //   const endSortPosition = quickSortHelper(
-  //     testArray,
-  //     compareFirst,
-  //     pixelIndexLength,
-  //     startIdx,
-  //     endIdx
-  //   );
-  //   expect(testArray).toEqual(new Uint8ClampedArray([0,0,0,0,1,0,0,0]))
-  // });
-  // test("sort helper sorts remaining 3 pixel values as well as selected", () => {
-  //   let testArray = new Uint8ClampedArray([1, 9, 9, 9, 0, 10, 10, 10]);
-  //   const startIdx = 0;
-  //   const endIdx = testArray.length;
-  //   const endSortPosition = quickSortHelper(
-  //     testArray,
-  //     compareFirst,
-  //     pixelIndexLength,
-  //     startIdx,
-  //     endIdx
-  //   );
-  //   expect(testArray).toEqual(new Uint8ClampedArray([0,10,10,10,1,9,9,9]))
-  // });
 
   test("sorts three pixels", () => {
     let testArray = new Uint8ClampedArray([2, 8, 8, 8, 3, 7, 7, 7, 1, 9, 9, 9]);
@@ -532,6 +508,105 @@ describe("Quick sort tests", () => {
     ]);
     let sortedCallback = jest.fn();
     quickSort(
+      testArray,
+      sortedCallback,
+      compareAll,
+      testSortPosition,
+      pixelIndexLength
+    );
+    expect(testArray).toEqual(
+      new Uint8ClampedArray([4, 3, 2, 1, 8, 7, 6, 5, 12, 11, 10, 9])
+    );
+  });
+});
+
+describe("Merge sort tests", () => {
+  let testSortPosition: null | number = null;
+  test("sorts three pixels", () => {
+    let testArray = new Uint8ClampedArray([2, 8, 8, 8, 3, 7, 7, 7, 1, 9, 9, 9]);
+    let sortedCallback = jest.fn();
+    const endSortPosition = mergeSort(
+      testArray,
+      sortedCallback,
+      compareFirst,
+      testSortPosition,
+      pixelIndexLength
+    );
+    expect(sortedCallback).toHaveBeenCalled();
+    expect(testArray).toEqual(
+      new Uint8ClampedArray([1, 9, 9, 9, 2, 8, 8, 8, 3, 7, 7, 7])
+    );
+  });
+  test("sorts three pixels with sortPosition as null", () => {
+    let testArray = new Uint8ClampedArray([2, 8, 8, 8, 3, 7, 7, 7, 1, 9, 9, 9]);
+    let sortedCallback = jest.fn();
+    const sortPositionStart = null;
+    const endSortPosition = mergeSort(
+      testArray,
+      sortedCallback,
+      compareFirst,
+      testSortPosition,
+      pixelIndexLength
+    );
+    expect(testArray).toEqual(
+      new Uint8ClampedArray([1, 9, 9, 9, 2, 8, 8, 8, 3, 7, 7, 7])
+    );
+  });
+  test("sorts three digit numbers", () => {
+    let testArray = new Uint8ClampedArray([
+      128, 200, 200, 200, 256, 300, 300, 300, 56, 100, 100, 100,
+    ]);
+    let sortedCallback = jest.fn();
+    const sortPositionStart = null;
+    const endSortPosition = mergeSort(
+      testArray,
+      sortedCallback,
+      compareFirst,
+      testSortPosition,
+      pixelIndexLength
+    );
+    expect(testArray).toEqual(
+      new Uint8ClampedArray([
+        56, 100, 100, 100, 128, 200, 200, 200, 256, 300, 300, 300,
+      ])
+    );
+  });
+  test("sort by second pixel value", () => {
+    let testArray = new Uint8ClampedArray([8, 2, 8, 8, 7, 3, 7, 7, 9, 1, 9, 9]);
+    let sortedCallback = jest.fn();
+    const sortPositionStart = null;
+    const endSortPosition = mergeSort(
+      testArray,
+      sortedCallback,
+      compareSecond,
+      testSortPosition,
+      pixelIndexLength
+    );
+    expect(testArray).toEqual(
+      new Uint8ClampedArray([9, 1, 9, 9, 8, 2, 8, 8, 7, 3, 7, 7])
+    );
+  });
+  test("sort by third pixel value", () => {
+    let testArray = new Uint8ClampedArray([8, 8, 2, 8, 7, 7, 3, 7, 9, 9, 1, 9]);
+    let sortedCallback = jest.fn();
+    const sortPositionStart = null;
+    const endSortPosition = mergeSort(
+      testArray,
+      sortedCallback,
+      compareThird,
+      testSortPosition,
+      pixelIndexLength
+    );
+    expect(testArray).toEqual(
+      new Uint8ClampedArray([9, 9, 1, 9, 8, 8, 2, 8, 7, 7, 3, 7])
+    );
+  });
+  test("sort by combined pixel value", () => {
+    let testArray = new Uint8ClampedArray([
+      8, 7, 6, 5, 12, 11, 10, 9, 4, 3, 2, 1,
+    ]);
+    let sortedCallback = jest.fn();
+    mergeSort(
       testArray,
       sortedCallback,
       compareAll,
---
+++ src/utils/algos/merge-sort.ts
@@ -0,0 +1,89 @@
+import copy from "./pixel-copy";
+import { SortAlgoTypes, SortDataTypes } from "./algoTypes";
+import { compareFnTypes } from "./pixel-comparison";
+
+const mergeSort: SortAlgoTypes = (
+  array,
+  sortedCallback,
+  compare,
+  sortPosition,
+  pixelIdxLength = 4,
+  renderLoops = 10
+) => {
+  let width: number =
+    typeof sortPosition === "number" ? sortPosition : pixelIdxLength;
+  let length = array.length;
+  for (let i = 0; i <= renderLoops; i++) {
+    while (width < length) {
+      let left: number = 0;
+      while (left < length) {
+        let right = Math.min(
+          left + (width * 2 - pixelIdxLength),
+          length - pixelIdxLength
+        );
+        let middle = Math.min(
+          left + width - pixelIdxLength,
+          length - pixelIdxLength
+        );
+        merge(array, left, right, middle, compare, pixelIdxLength);
+        left += width * 2;
+      }
+      width *= 2;
+    }
+    if (width >= length) {
+      sortedCallback();
+      break;
+    }
+  }
+  return width;
+};
+
+const merge = (
+  array: Uint8ClampedArray,
+  left: number,
+  right: number,
+  middle: number,
+  compare: compareFnTypes,
+  pixelIdxLength: number
+) => {
+  let n1 = middle - left + pixelIdxLength;
+  let n2 = right - middle;
+  let L: number[] = new Array(n1).fill(0);
+  let R: number[] = new Array(n2).fill(0);
+
+  for (let i = 0; i < n1; i++) {
+    L[i] = array[left + i];
+  }
+  for (let i = 0; i < n2; i++) {
+    R[i] = array[middle + i + pixelIdxLength];
+  }
+
+  let i: number = 0;
+  let j: number = 0;
+  let k: number = left;
+
+  while (i < n1 && j < n2) {
+    if (compare(L, i) <= compare(R, j)) {
+      copy(L, i, array, k);
+      i += pixelIdxLength;
+    } else {
+      copy(R, j, array, k);
+      j += pixelIdxLength;
+    }
+    k += pixelIdxLength;
+  }
+
+  while (i < n1) {
+    copy(L, i, array, k);
+    i += pixelIdxLength;
+    k += pixelIdxLength;
+  }
+
+  while (j < n2) {
+    copy(R, j, array, k);
+    j += pixelIdxLength;
+    k += pixelIdxLength;
+  }
+};
+
+export default mergeSort;
