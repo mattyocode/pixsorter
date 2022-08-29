c8258e7ef85cd87f334fb65d4bf6eb788b22a4ad
Matt <63904102+mattyocode@users.noreply.github.com>
2022-08-29 15:26:49 +0100

    move algo descriptions into own file and import

--- src/store/algoData.ts
+++ src/store/algoData.ts
@@ -1,5 +1,10 @@
 import { sortAlgoTypes } from "../utils/algos/algoTypes";
 import {
+  DescriptionType,
+  algoDescriptions,
+  sortByDescriptions,
+} from "./descriptions";
+import {
   bubbleSort,
   insertionSort,
   selectionSort,
@@ -18,31 +23,6 @@ export type AlgoItemType = {
   value: string;
   function: sortAlgoTypes;
   description: DescriptionType;
-};
-
-type DescriptionType = { heading: string; bodyCopy: string };
-
-const algoDescriptions: { [key: string]: DescriptionType } = {
-  quick: {
-    heading: "Avg. time: O(n log n) | Avg Space: O(n)",
-    bodyCopy:
-      "Find the correct position of a pivot point in the array (i.e. all values before are lower, and all following are higher), then divide the array at that point. Repeat until the subarrays are so small that moving the pivot sorts them.",
-  },
-  bubble: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Start at the beginning. Check if the next value in the array is lower: if so, swap with the current pixel; if not, don't. Move to the next value. This moves the largest number to the last index. Repeat the whole process, stopping one index earlier each time.",
-  },
-  insertion: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Consequat in dolore in nulla. Adipisicing excepteur velit occaecat consequat non nostrud consequat labore. Eu aute consectetur ea dolore minim aliqua esse. Amet excepteur amet velit aliquip consequat non labore ad laboris id.",
-  },
-  selection: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Consequat in dolore in nulla. Adipisicing excepteur velit occaecat consequat non nostrud consequat labore. Eu aute consectetur ea dolore minim aliqua esse. Amet excepteur amet velit aliquip consequat non labore ad laboris id.",
-  },
 };
 
 export const AlgoOptions: AlgoItemType[] = [
@@ -79,32 +59,9 @@ export type SortByItemType = {
   description: DescriptionType;
 };
 
-const sortByDescriptions: { [key: string]: DescriptionType } = {
-  combined: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Consequat in dolore in nulla. Adipisicing excepteur velit occaecat consequat non nostrud consequat labore. Eu aute consectetur ea dolore minim aliqua esse. Amet excepteur amet velit aliquip consequat non labore ad laboris id.",
-  },
-  red: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Consequat in dolore in nulla. Adipisicing excepteur velit occaecat consequat non nostrud consequat labore. Eu aute consectetur ea dolore minim aliqua esse. Amet excepteur amet velit aliquip consequat non labore ad laboris id.",
-  },
-  green: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Consequat in dolore in nulla. Adipisicing excepteur velit occaecat consequat non nostrud consequat labore. Eu aute consectetur ea dolore minim aliqua esse. Amet excepteur amet velit aliquip consequat non labore ad laboris id.",
-  },
-  blue: {
-    heading: "Avg. time: O(n²) | Avg Space: O(1)",
-    bodyCopy:
-      "Consequat in dolore in nulla. Adipisicing excepteur velit occaecat consequat non nostrud consequat labore. Eu aute consectetur ea dolore minim aliqua esse. Amet excepteur amet velit aliquip consequat non labore ad laboris id.",
-  },
-};
-
 export const SortByOptions: SortByItemType[] = [
   {
-    label: "Combined RGB",
+    label: "Combined",
     value: "brightness",
     function: compareBrightness,
     description: sortByDescriptions.combined,
