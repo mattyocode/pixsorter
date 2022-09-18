df0fce0c03804e5eb7271fc8be403d33a39be1e7
Matt <63904102+mattyocode@users.noreply.github.com>
2022-09-18 13:59:34 +0100

    add types

--- src/hooks/use-http.ts
+++ src/hooks/use-http.ts
@@ -1,31 +1,36 @@
 import { useState, useCallback } from "react";
 
+type sendRequestTypes = (requestConfig: any, applyData: any) => Promise<void>;
+
 const useHttp = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean | null>(null);
 
-  const sendRequest = useCallback(async (requestConfig, applyData) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : "GET",
-        headers: requestConfig.headers ? requestConfig.headers : {},
-        body: requestConfig ? JSON.stringify(requestConfig.body) : null,
-      });
+  const sendRequest: sendRequestTypes = useCallback(
+    async (requestConfig, applyData) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig ? JSON.stringify(requestConfig.body) : null,
+        });
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+        applyData(data);
+      } catch (err: any) {
+        setError(err.message || "Something went wrong!");
       }
-
-      const data = await response.json();
-      console.log(data);
-      applyData(data);
-    } catch (err: any) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  }, []);
+      setIsLoading(false);
+    },
+    []
+  );
   return {
     isLoading,
     error,
