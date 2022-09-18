4ddc7576e7a7b3a371789928659878ee59aad1c2
Matt <63904102+mattyocode@users.noreply.github.com>
2022-09-18 13:41:15 +0100

    add error boundary component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,36 +7,39 @@ import { Heading } from "../components/Heading";
 import { ImageUI } from "../components/ImageUI";
 import { Controls } from "../components/Controls";
 import { Footer } from "../components/Footer";
+import ErrorBoundary from "../components/ErrorBoundary";
 import AlgoProvider from "../store/AlgoProvider";
 import SortingProvider from "../store/SortingProvider";
 import UIProvider from "../store/UIProvider";
 
 const Home: NextPage = () => {
   return (
-    <AlgoProvider>
-      <SortingProvider>
-        <UIProvider>
-          <Head>
-            <title>PixSorter</title>
-            <meta name="description" content="Sort images with algorithms" />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <main>
-            <ImageBackground src="/img/background.jpg">
-              <Heading
-                title="PixSorter"
-                subhead="Watch algorithms sort pixels."
-              />
-              <ImageUI />
-              <Controls />
-              <Footer>
-                <a href="https://github.com/mattyocode">mattyocode</a>
-              </Footer>
-            </ImageBackground>
-          </main>
-        </UIProvider>
-      </SortingProvider>
-    </AlgoProvider>
+    <ErrorBoundary>
+      <AlgoProvider>
+        <SortingProvider>
+          <UIProvider>
+            <Head>
+              <title>PixSorter</title>
+              <meta name="description" content="Sort images with algorithms" />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+              <ImageBackground src="/img/background.jpg">
+                <Heading
+                  title="PixSorter"
+                  subhead="Watch algorithms sort pixels."
+                />
+                <ImageUI />
+                <Controls />
+                <Footer>
+                  <a href="https://github.com/mattyocode">mattyocode</a>
+                </Footer>
+              </ImageBackground>
+            </main>
+          </UIProvider>
+        </SortingProvider>
+      </AlgoProvider>
+    </ErrorBoundary>
   );
 };
 
